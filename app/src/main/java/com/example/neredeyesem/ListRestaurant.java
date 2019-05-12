9fc84900fb19b864acacdecdf474fd07ad488c15
Erdem <i.erdemcalisir@gmail.com>
2019-05-12 22:42:08 +0300

    add other activity and acyivity.xml files

---
+++ app/src/main/java/com/example/neredeyesem/ListRestaurant.java
@@ -0,0 +1,13 @@
+package com.example.neredeyesem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListRestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_restaurant);
+    }
+}
